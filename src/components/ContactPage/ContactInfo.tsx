5edabceb27f811da2bb1df6f3ae0db710298d61e
Mckenzie-Richard <richardbrightasiedu@gmail.com>
2025-05-20 22:25:47 +0000

    update phone number in ContactInfo component and add Vercel configuration

--- src/components/ContactPage/ContactInfo.tsx
+++ src/components/ContactPage/ContactInfo.tsx
@@ -88,7 +88,7 @@ const ContactInfo = () => {
           </div>
           <div>
             <h4 className="text-navy font-medium mb-1">Phone</h4>
-            <a href="tel:+233501234567" className="text-indigo hover:text-navy transition-colors duration-300 underline-animated">
+            <a href="tel:+233543876146" className="text-indigo hover:text-navy transition-colors duration-300 underline-animated">
               +233 54 387 6146
             </a>
           </div>
